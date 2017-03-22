428b0f72af3efebe499f85799c8f239c16f50d51
Jake <jake@alabaster.io>
2017-03-21 20:01:30 -0700

    More Updates

---
+++ home.php
@@ -0,0 +1,108 @@
+<?php include('header.php');?>
+<section class="hero">
+  <div class="container">
+    <h1>At FamilyEQ, we believe that every family, especially those faced with extra needs, has hidden potential for growth, joy and satisfaction.</h1>
+    <div class="hero-container row">
+      <div class="col-xs-12 col-sm-5 col-sm-push-7 col-md-6 col-md-push-6 hero-image" style="background-image:url('assets/img/posts-landing.jpg');">
+      </div>
+      <div class="col-xs-12 col-sm-7 col-sm-pull-5 col-md-6 col-md-pull-6 hero-content-block">
+        <div class="hero-content">
+          <p><span class="blue-bold">FamilyEQ.com</span> was created to encourage families and empower caregivers. We offer assessment tools and educational resources designed to help improve a family's relational health and overall well-being so everyone can flourish!</p>
+          <button class="content-button"><a href="#">Get Started</a></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="hm-four">
+  <div class="container">
+    <div class="col-xs-12 col-sm-6 col-md-3 point">
+      <div class="point-icon">
+        <object type="image/svg+xml" data="assets/img/icons/Functioning.svg" alt="Functioning">
+          <img src="assets/img/icons/png/Functioning.png" alt="No SVG support for Functioning icon">
+        </object>
+      </div>
+      <div class="point-content">
+        <h2>Functioning Well</h2>
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-3 point">
+      <div class="point-icon">
+        <object type="image/svg+xml" data="assets/img/icons/Relating.svg" alt="Relating">
+          <img src="assets/img/icons/png/Relating.png" alt="No SVG support for Relating icon">
+        </object>
+      </div>
+      <div class="point-content">
+        <h2>Relating Well</h2>
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-3 point">
+      <div class="point-icon">
+        <object type="image/svg+xml" data="assets/img/icons/Unwinding.svg" alt="Unwinding">
+          <img src="assets/img/icons/png/Unwinding.png" alt="No SVG support for Unwinding icon">
+        </object>
+      </div>
+      <div class="point-content">
+        <h2>Unwinding Well</h2>
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-3 point">
+      <div class="point-icon">
+        <object type="image/svg+xml" data="assets/img/icons/Navigating.svg" alt="Navigating">
+          <img src="assets/img/icons/png/Navigating.png" alt="No SVG support for Navigating icon">
+        </object>
+      </div>
+      <div class="point-content">
+        <h2>Navigating Well</h2>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="posts-section">
+  <div class="container">
+    <div class="col-xs-12 col-sm-6">
+      <div class="post">
+        <div class="post-image" style="background-image:url('assets/img/post-1.jpg')" alt="Post 1"></div>
+        <div class="post-content">
+          <div class="post-header">
+            <div class="post-read-time">
+              <p>Read Time: 3 Minutes</p>
+            </div>
+          </div>
+          <div class="post-title">
+            <h3>Article Title</h3>
+          </div>
+          <div class="post-excerpt">
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+          </div>
+          <div class="post-link">
+            <a href="#" alt="Post Title">View Full Article</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+      <div class="post">
+        <div class="post-image" style="background-image:url('assets/img/post-2.jpg')" alt="Post 2"></div>
+        <div class="post-content">
+          <div class="post-header">
+            <div class="post-read-time">
+              <p>Read Time: 3 Minutes</p>
+            </div>
+          </div>
+          <div class="post-title">
+            <h3>Article Title</h3>
+          </div>
+          <div class="post-excerpt">
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+          </div>
+          <div class="post-link">
+            <a href="#" alt="Post Title">View Full Article</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include('footer.php');?>
